d4277eb04d6a939cc87c87f7ecad82ccbc22fd07
Tejas Sawant <tejassawant54@users.noreply.github.com>
2016-05-30 17:35:39 -0400

    Update phn_spec.js

--- test/phn_spec.js
+++ test/phn_spec.js
@@ -6,7 +6,7 @@ describe('filter tests', function() {
     beforeEach(module('myApp'));
 
     it('should format phone number', inject(function(phnNumFormatFilter) {
-            expect(phnNumFormatFilter('5857336560')).toBe('+1 (585) 733-6560');
+            expect(phnNumFormatFilter('+15857336560')).toBe('+1 (585) 733-6560');
         })
     );
 });
